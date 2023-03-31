be6d4d7892a25fc2ffd7f840cd2e98aa3f150f74
Ekankth <ekankth@gmail.com>
2023-03-31 23:44:53 +0530

    Ekankth

---
+++ EkankthRobot/modules/alive.py
@@ -0,0 +1,35 @@
+import random
+
+from pyrogram import __version__ as pyrover
+from telegram import __version__ as telever
+from telethon import Button
+from telethon import __version__ as tlhver
+
+from EkankthRobot import OWNER_USERNAME, SUPPORT_CHAT, dispatcher
+from EkankthRobot import telethn as tbot
+from EkankthRobot.events import register
+
+PHOTO = [
+    "https://telegra.ph/file/b2b4fee33e6c7d2a5651a.jpg",
+    "https://telegra.ph/file/b2b4fee33e6c7d2a5651a.jpg",
+]
+
+
+@register(pattern=("/alive"))
+async def awake(event):
+    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ {dispatcher.bot.first_name}**\n━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝄟🐰❤️⃝🇻​​​​​🇮​​​​​≛⃝🇵​​​​​💞🐰𝄟](https://t.me/THE_VIP_BOY)** \n\n"
+    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
+    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
+    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
+    BUTTON = [
+        [
+            Button.url("👑𝐊𝐈𝐍𝐆👑", f"https://t.me/VIP_CREATORS"),
+            Button.url("​👸𝐐𝐔𝐄𝐄𝐍👸", f"https://t.me/LOVERS_DUNIA"),
+        ]
+    ]
+    ran = random.choice(PHOTO)
+    await tbot.send_file(event.chat_id, ran, caption=TEXT, buttons=BUTTON)
+
+
+__mod_name__ = "👑𝐀𝐋𝐈𝐕𝐄👸"
